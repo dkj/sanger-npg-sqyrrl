e3795c5940639c94f2ca9c351c63be7342f33bc3
David K. Jackson <david.jackson@sanger.ac.uk>
2025-02-08 15:37:30 +0000

    Initial mockoidc server use in tests

--- server/handlers_test.go
+++ server/handlers_test.go
@@ -29,6 +29,7 @@ import (
 	"github.com/cyverse/go-irodsclient/irods/connection"
 	ifs "github.com/cyverse/go-irodsclient/irods/fs"
 	"github.com/cyverse/go-irodsclient/irods/types"
+	"github.com/oauth2-proxy/mockoidc"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -109,11 +110,12 @@ var _ = Describe("iRODS Get Handler", func() {
 				}, NodeTimeout(time.Second*2))
 
 				When("the data object does not have read permissions for the public group", func() {
-					It("should return Forbidden", func(ctx SpecContext) {
+					It("should return a redirect to the auth server", func(ctx SpecContext) {
 						rec := httptest.NewRecorder()
 						handler.ServeHTTP(rec, r)
 
-						Expect(rec.Code).To(Equal(http.StatusForbidden))
+						Expect(rec.Code).To(Equal(http.StatusFound))
+						Expect(rec.Header().Get("Location")).To(ContainSubstring(mockoidcServer.AuthorizationEndpoint()))
 					}, SpecTimeout(specTimeout))
 				})
 
@@ -323,4 +325,53 @@ var _ = Describe("iRODS Get Handler", func() {
 			})
 		})
 	})
+})
+
+var _ = Describe("Authentication Handler", func() {
+	When("Logging in to Sqyrrl", func() {
+		var r *http.Request
+		var handler http.Handler
+		var err error
+		var ws *httptest.ResponseRecorder
+
+		BeforeEach(func(ctx SpecContext) {
+			handler, err = sqyrrlServer.GetHandler(server.EndpointLogin)
+			Expect(err).NotTo(HaveOccurred())
+
+			postURL := server.EndpointLogin
+			r, err = http.NewRequest("POST", postURL, nil)
+			Expect(err).NotTo(HaveOccurred())
+		}, NodeTimeout(time.Second*2))
+
+		It("should return a 302 redirect to OIDC server", func(ctx SpecContext) {
+			ws = httptest.NewRecorder()
+			handler.ServeHTTP(ws, r)
+
+			Expect(ws.Code).To(Equal(http.StatusFound))
+			Expect(ws.Header().Get("Location")).To(ContainSubstring(mockoidcServer.AuthorizationEndpoint()))
+		})
+
+		When("contacting the OIDC server", func() {
+			BeforeEach(func(ctx SpecContext) {
+				mockoidcServer.UserQueue.Push(&mockoidc.MockUser{
+					Email: "someuser@somewhere.com",
+				})
+			})
+
+			It("should return a 302 redirect to the Sqyrrl auth callback", func(ctx SpecContext) {
+				httpclient := http.Client{
+					CheckRedirect: func(req *http.Request, via []*http.Request) error {
+						// special case to stop the redirect chain
+						return http.ErrUseLastResponse
+					},
+				}
+				wo, err := httpclient.Get(ws.Header().Get("Location"))
+				Expect(err).NotTo(HaveOccurred())
+				Expect(wo.StatusCode).To(Equal(http.StatusFound))
+				Expect(wo.Header.Get("Location")).To(ContainSubstring(server.EndpointAuthCallback))
+			})
+			// Can continue here with Sqyrrl's auth callbacl handler
+		})
+	})
+
 })
